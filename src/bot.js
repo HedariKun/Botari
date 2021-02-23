4a5a7bb5124332ad7930eabb3b2965d8f48136b2
HedariKun <36459011+HedariKun@users.noreply.github.com>
2021-02-24 01:12:06 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -5,5 +5,5 @@ const token = "1670239238:AAHpccOKbC6IOOe0e6T8hwOfOoaWgBddQcQ"
 const bot = new telegramBot(token, {polling: true})
 
 bot.onText(/\/pingall/, (msg) => {
-    bot.sendMessage(msg.chat.id, "@Thikra_A_A @Khadija_ahmed_abbas @ameerthameer1  @rusm2  @Z_767  @dd20888  𝑓𝑎♪𝑖𝑚𝑎☁️  @ha_gk18  @amoxiHEDARIN  @Hawraa_jasim  @rahma_ayaad  @H_d_i_a  @m_a_7m  @Khrstomn774  @IQ_Rudy  @h8yat  @Zaid56_iQ  @jung2003  @fernweh2003  @AYLLA_H @DAYDrEaMEr003 @dinawaleed @H102na  كاعدين ؟")
+    bot.sendMessage(msg.chat.id, "@Thikra_A_A @Khadija_ahmed_abbas @Duaa_g @da_l9 @ameerthameer1  @rusm2  @Z_767  @dd20888  𝑓𝑎♪𝑖𝑚𝑎☁️  @ha_gk18  @amoxiHEDARIN  @Hawraa_jasim  @rahma_ayaad  @H_d_i_a  @m_a_7m  @Khrstomn774  @IQ_Rudy  @h8yat  @Zaid56_iQ  @jung2003  @fernweh2003  @AYLLA_H @DAYDrEaMEr003 @dinawaleed @H102na  كاعدين ؟")
 })
